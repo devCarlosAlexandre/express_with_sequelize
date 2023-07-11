184b1633460925af6e1702742e7db29d20e6d95b
devCarlosAlexandre <carloalexandresousa@gmail.com>
2023-07-11 18:54:10 -0300

    feat: :sparkles: validação customizada

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -19,7 +19,14 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        functionValidate: function (dado) {
+          if (dado.length < 3) throw new Error('O campo nome deve ter mais de caracteres')
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     email: {
       type: DataTypes.STRING,
