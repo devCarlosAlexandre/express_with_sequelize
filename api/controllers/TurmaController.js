620ac8490666b2c6f3210a89c05f709336714f87
devCarlosAlexandre <carloalexandresousa@gmail.com>
2023-07-12 08:25:48 -0300

    feat: :sparkles: criado filtro por data

--- api/controllers/TurmaController.js
+++ api/controllers/TurmaController.js
@@ -1,9 +1,16 @@
 const database = require("../models");
+const { Op } = require("sequelize");
 
 class TurmaController {
+
     static async pegarTodasTurmas(req, res) {
+        const { data_inicial, data_final } = req.query;
+        const where = {};
+        data_inicial || data_final ? where.data_inicio = {} : null;
+        data_inicial ? where.data_inicio[Op.gte] = data_inicial : null;
+        data_final ? where.data_inicio[Op.lte] = data_final : null;
         try {
-            const todasTurmas = await database.Turmas.findAll();
+            const todasTurmas = await database.Turmas.findAll({ where });
             return res.status(200).json(todasTurmas);
         } catch (error) {
             return res.status(500).json(error.message);
